b487bf56c8f0875bf8a5286d2220556e05c17a46
Ben <allenben@protonmail.com>
2019-07-19 12:57:22 -0500

    Finalized Styling

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -19,9 +19,14 @@ const CharacterShowcaseStyle = styled.div`
   height: 40%;
   border: solid;
   border-radius: 50%;
+  background-color: tan;
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  :hover { 
+    background-color: lightblue;
+  }
   
 }
 `

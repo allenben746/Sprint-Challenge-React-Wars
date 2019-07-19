e8a6cd26eb052cb9ab91eddd71471ae217f2e305
Ben <allenben@protonmail.com>
2019-07-19 12:23:34 -0500

    Converted styling to styled-components

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,6 +2,26 @@ import React from 'react';
 import Character from './components/Characters';
 
 import './App.css';
+import styled from 'styled-components';
+
+const CharacterShowcaseStyle = styled.div`
+.mainContainer {
+  display: flex;
+  justify-content: center;
+  padding: 10% 0% 10% 0%;
+}
+
+.CharacterContainer {
+  width: 55%;
+  height: 40%;
+  border: solid;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+}
+
+`
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -13,7 +33,7 @@ const App = () => {
 
   return (
     <div>
-      <Character />
+     <CharacterShowcaseStyle><Character /></CharacterShowcaseStyle>
     </div>
   );
 }
--- starwars/src/components/CharacterShowcase.js
+++ starwars/src/components/CharacterShowcase.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-import './showcase.css'
 
 
 const CharacterShowcase = (props) => {
